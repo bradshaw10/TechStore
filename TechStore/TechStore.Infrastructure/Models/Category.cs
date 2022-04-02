e8bc26f254f40086dfead9a1989fa9f420d7d43b
bradshaw10 <eoghan.bradshaw@gmail.com>
2022-04-02 16:10:03 +0400

    Tables added

--- TechStore/TechStore.Infrastructure/Models/Category.cs
+++ TechStore/TechStore.Infrastructure/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace TechStore.Infrastructure.Models
     public class Category
     {
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
--- TechStore/TechStore.Infrastructure/Models/Product.cs
+++ TechStore/TechStore.Infrastructure/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace TechStore.Infrastructure.Models
     public class Product
     {
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Barcode { get; set; }
         public string Description { get; set; }
         public double Weight { get; set; }
