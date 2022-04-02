95dcff88af04d14aace1542f39402dd16cb64473
bradshaw10 <eoghan.bradshaw@gmail.com>
2022-04-02 13:50:03 +0400

    initial commit

---
+++ TechStore/TechStore.Infrastructure/Context/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.Infrastructure.Models;
+
+namespace TechStore.Infrastructure.Context
+{
+    public class ProductContext: DbContext
+    {
+        public ProductContext(DbContextOptions options): base(options) { }
+    
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ TechStore/TechStore.Infrastructure/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechStore.Infrastructure.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public string Description { get; set; }
+        public double Weight { get; set; }
+        public Status Status { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+
+    public enum Status
+    {
+        Sold,
+        inStock,
+        Damaged
+    }
+}
